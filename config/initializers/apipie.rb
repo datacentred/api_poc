6c6c4c694504c0153d7dee152d3ee90a07974b1b
Sean Handley <sean.handley@gmail.com>
2016-05-30 14:23:07 +0100

    More meta info.

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -1,8 +1,9 @@
 Apipie.configure do |config|
-  config.app_name                = "ApiPoc"
+  config.app_name                = "DataCentred Dashboard API"
   config.api_base_url            = ""
   config.doc_base_url            = "/docs"
   config.default_version         = Rails.configuration.latest_api_version.downcase.to_s
   # where is your API defined?
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/**/*.rb"
+  config.app_info                = "User and Project Management API"
 end
