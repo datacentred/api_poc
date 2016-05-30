865a802b22a51d3cbcc626edae6693f1e88dc097
Sean Handley <sean.handley@gmail.com>
2016-05-30 20:55:14 +0100

    Tweaks.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   apipie
+
+  root :to => redirect('/docs')
   
   Rails.configuration.api_versions.each do |version|
     scope module: version.downcase, constraints: ApiVersionConstraint.new(version: version) do
