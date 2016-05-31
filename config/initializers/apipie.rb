ba53f8ecedf9e062c6b7a416ad24d5ba67a6ffdf
Sean Handley <sean.handley@gmail.com>
2016-05-31 10:29:23 +0100

    Use Heroku for testing.

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -2,7 +2,8 @@ Apipie.configure do |config|
   config.app_name                = "DataCentred Dashboard API"
   config.doc_base_url            = "/docs"
   config.default_version         = Rails.configuration.latest_api_version.downcase.to_s
-  config.api_base_url            = "https://api.datacentred.io"
+  # config.api_base_url            = "https://api.datacentred.io"
+  config.api_base_url            = "https://calm-cove-59936.herokuapp.com"
   # where is your API defined?
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/**/*.rb"
   config.app_info                = "User and Project Management API"
