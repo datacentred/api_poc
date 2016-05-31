7eab345bf326d495bc6b44922cab0b1aded6c458
Sean Handley <sean.handley@gmail.com>
2016-05-31 10:01:17 +0100

    Better default settings.

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -2,8 +2,10 @@ Apipie.configure do |config|
   config.app_name                = "DataCentred Dashboard API"
   config.doc_base_url            = "/docs"
   config.default_version         = Rails.configuration.latest_api_version.downcase.to_s
-  config.api_base_url            = ""
+  config.api_base_url            = "https://api.datacentred.io"
   # where is your API defined?
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/**/*.rb"
   config.app_info                = "User and Project Management API"
+  config.markup                  = Apipie::Markup::Markdown.new
+  config.validate                = false
 end
