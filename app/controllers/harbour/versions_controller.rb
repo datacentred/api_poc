857c4b0c04402d65473bb139f990af086c826638
Sean Handley <sean@DixieBook.local>
2017-01-20 10:33:36 +0000

    Rename status to "versions".

---
+++ app/controllers/harbour/versions_controller.rb
@@ -0,0 +1,29 @@
+require_dependency "harbour/application_controller"
+
+module Harbour
+  class VersionsController < ApplicationController
+    def index
+      respond_with(
+        {
+          "versions": supported_versions.map do |v|
+            {
+              "id": v.to_s.gsub("V",""),
+              "status": (v == latest_version ? "CURRENT" : "SUPPORTED"),
+              "links": [{"href": Harbour::Engine.config.public_url, "rel": "self"}]
+            }
+          end
+        }
+      )
+    end
+
+    private
+
+    def supported_versions
+      Harbour::Engine.config.api_versions.sort.reverse
+    end
+
+    def latest_version
+      Harbour::Engine.config.latest_api_version
+    end
+  end
+end
