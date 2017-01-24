9bd8e63a6620fdf5a70122481db32aa9ca279b38
Sean Handley <sean@dixiebook.home>
2017-01-24 17:32:47 +0000

    Set an API version in the response headers.

--- app/controllers/harbour/authorized_controller.rb
+++ app/controllers/harbour/authorized_controller.rb
@@ -4,7 +4,7 @@ module Harbour
   class AuthorizedController < ApplicationController
     include ActionController::HttpAuthentication::Token::ControllerMethods
 
-    before_action :current_user
+    before_action :set_api_version_header, :current_user
 
     def current_user
       authenticate_or_request_with_http_token do |token, _|
@@ -24,6 +24,25 @@ module Harbour
 
     def scoped_projects
       Project.where(organization: current_organization).includes(:users)
+    end
+
+    private
+
+    def set_api_version_header
+      version = request.headers.fetch(:accept)&.scan(/version=(\d+)/)&.first&.first
+      if version
+        unless Harbour::Engine.config.api_versions.include?("V#{version}".to_sym)
+          render json: {
+            error: "Unknown API version #{version}.",
+            links: [
+              {href: Harbour::Engine.config.public_url, rel: 'help'}
+            ] 
+          }, status: 406
+        end
+      else
+        version = Harbour::Engine.config.latest_api_version.to_s.scan(/V(\d+)/).first.first
+      end
+      response.headers['X-API-Version'] = version
     end
   end
 end
