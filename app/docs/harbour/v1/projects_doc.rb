3c52b51037922f61972fc8426a151bff2fe3bed6
Sean Handley <sean@dixiebook.home>
2017-01-25 17:48:39 +0000

    Docs for projects.

--- app/docs/harbour/v1/projects_doc.rb
+++ app/docs/harbour/v1/projects_doc.rb
@@ -14,58 +14,133 @@ module Harbour
       end
 
       def_param_group :project do
-        param :project, Hash, desc: 'Project information', required: true do
-          param :name, String, desc: 'Name of the project', required: true
+        param :project, Hash, desc: 'Project information', required: true, :action_aware => true do
+          param :name, String, desc: 'Name of the project', required: true, as: :create
         end
       end
 
-      api :POST, '/projects', 'Create project'
-      description 'Create project with specifed project params'
-      param_group :project
+      api :POST, '/projects', 'Create a new project'
+      description 'Create a new DataCentred cloud project (backed by OpenStack).'
+      param_group :project, as: :create
+      error 201, "Created successfully"
+      error 422, "Failed validation. Details of failure returned in body."
       
       example <<-EOS
-    #{curl_method('projects', '1', {'X': 'POST', 'd': '{"project": {"foo": "bar"}}'})}
-      EOS
+# Create new project
+#{curl_method('projects', '1', {'X': 'POST', 'd': '{"project": {"name": "wyld_stalyns"}}'})}
+{
+  "project":{
+    "uuid":"24c1de959cb943e0bf11e5ca6c8f8ad8",
+    "name":"wyld_stalyns",
+    "links":[
+      {
+        "href":"#{Harbour::Engine.config.public_url}/projects/24c1de959cb943e0bf11e5ca6c8f8ad8",
+        "rel":"self"
+      }
+    ],
+    "users":[]
+  }
+}
+EOS
+example <<-EOS
+# Create new project when name is already taken
+#{curl_method('projects', '1', {'X': 'POST', 'd': '{"project": {"name": "already_taken"}}'})}
+{
+  "errors":[
+    {
+      "resource": "project",
+      "field": "name",
+      "detail": "Project name is already in use."
+    }
+  ]
+}
+EOS
       def create ; end
 
-      api :GET, '/projects', 'List projects'
-      description <<-EOS
-        Foo
-      EOS
+      api :GET, '/projects', 'List all available projects'
+      description "Show a list of all the projects."
+      error 200, "Success"
       example <<-EOS
-    #{curl_method('projects','1') }
-    [
-      {
-        "full_name": "Foo",
-        "age": 25
-      }
-    ]
+# List all available projects
+#{curl_method('projects','1') }
+{
+  "projects":[
+    {
+      "uuid": "24c1de959cb943e0bf11e5ca6c8f8ad8",
+      "name": "wyld_stalyns",
+      "links":[
+        {
+          "href": "http://localhost:3000/api/projects/24c1de959cb943e0bf11e5ca6c8f8ad8",
+          "rel": "self"
+        }
+      ],
+      "users":[]
+    }
+  ]
+}
       EOS
       def index ; end
 
-      api :PUT, '/projects/:uuid', 'Update project'
-      description 'Update specified project information'
-      param :uuid, String, required: true
-      param_group :project
-      error 404, "Missing"
+      api :GET, '/projects/:uuid', 'Show project'
+      description 'Show specified project'
+      param :uuid, String, desc: 'The unique identifier for this project', required: true
+      error 200, "Success"
+      error 404, "Project UUID couldn't be found"
       example <<-EOS
-    #{curl_method('projects/1', '1', {'X': 'PUT', 'd': '{"project": {"foo": "bar"}}'})}
-      EOS
-      def update ; end
-
-      api :GET, '/projects/:uuid', 'Get project'
-      description 'Retrieve specified project information'
-      param :uuid, String, required: true
-      example <<-EOS
-    #{curl_method('projects/1', '1')}
+# Show specified project
+#{curl_method('projects/24c1de959cb943e0bf11e5ca6c8f8ad8', '1')}
+{
+  "project":{
+    "uuid": "24c1de959cb943e0bf11e5ca6c8f8ad8",
+    "name": "wyld_stalyns",
+    "links":[
+      {
+        "href": "http://localhost:3000/api/projects/24c1de959cb943e0bf11e5ca6c8f8ad8",
+        "rel": "self"
+      }
+    ],
+    "users":[]
+  }
+}
       EOS
       def show ; end
 
-      api :DELETE, '/projects/:uuid', 'Delete project'
-      description 'Remove specified project'
-      param :uuid, String, required: true
+      api :PUT, '/projects/:uuid', 'Update a project'
+      description 'Update specified project params'
+      param :uuid, String, desc: 'The unique identifier for this project', required: true
+      param_group :project, as: :name
+      error 200, "Updated project successfully"
+      error 404, "Project UUID couldn't be found"
+      error 422, "Failed validation. Details of failure returned in body."
       example <<-EOS
-    #{curl_method('projects/1', '1', {'X': 'DELETE'})}
+# Rename project
+#{curl_method('projects/24c1de959cb943e0bf11e5ca6c8f8ad8', '1', {'X': 'PUT', 'd': '{"project": {"name": "wyld_stalyns_rock"}}'})}
+{
+  "project":{
+    "uuid": "24c1de959cb943e0bf11e5ca6c8f8ad8",
+    "name": "wyld_stalyns_rock",
+    "links":[
+      {
+        "href": "http://localhost:3000/api/projects/24c1de959cb943e0bf11e5ca6c8f8ad8",
+        "rel": "self"
+      }
+    ],
+    "users":[]
+  }
+}
+      EOS
+      def update ; end
+
+      api :DELETE, '/projects/:uuid', 'Delete project'
+      description 'Permanently removes the specified project.'
+      param :uuid, nil, desc: 'The unique identifier for this project', required: true
+      error 204, "Removed project successfully"
+      error 404, "Project UUID couldn't be found"
+      error 422, "Couldn't delete project. Details of failure returned in body."
+      example <<-EOS
+# Delete project
+#{curl_method('projects/24c1de959cb943e0bf11e5ca6c8f8ad8', '1', {'X': 'DELETE', 'I': nil})}
+HTTP/1.1 204 No Content
       EOS
       def destroy ; end
     end
