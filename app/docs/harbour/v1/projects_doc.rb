b1f515c9816107cfe92b668efdec06973261f010
Sean Handley <sean@dixiebook.home>
2017-01-25 18:26:40 +0000

    Moar documentation!

--- app/docs/harbour/v1/projects_doc.rb
+++ app/docs/harbour/v1/projects_doc.rb
@@ -14,20 +14,21 @@ module Harbour
       end
 
       def_param_group :project do
-        param :project, Hash, desc: 'Project information', required: true, :action_aware => true do
-          param :name, String, desc: 'Name of the project', required: true, as: :create
+        param   :project, Hash,   desc: 'Project information', required: true, action_aware: true do
+          param :name,    String, desc: 'Name of the project', required: true, as: :create
         end
       end
 
       api :POST, '/projects', 'Create a new project'
       description 'Create a new DataCentred cloud project (backed by OpenStack).'
       param_group :project, as: :create
-      error 201, "Created successfully"
+      error 201, "Created successfully."
       error 422, "Failed validation. Details of failure returned in body."
       
       example <<-EOS
 # Create new project
 #{curl_method('projects', '1', {'X': 'POST', 'd': '{"project": {"name": "wyld_stalyns"}}'})}
+# =>
 {
   "project":{
     "uuid":"24c1de959cb943e0bf11e5ca6c8f8ad8",
@@ -45,6 +46,7 @@ EOS
 example <<-EOS
 # Create new project when name is already taken
 #{curl_method('projects', '1', {'X': 'POST', 'd': '{"project": {"name": "already_taken"}}'})}
+# =>
 {
   "errors":[
     {
@@ -62,7 +64,8 @@ EOS
       error 200, "Success"
       example <<-EOS
 # List all available projects
-#{curl_method('projects','1') }
+#{curl_method('projects','1')}
+# =>
 {
   "projects":[
     {
@@ -85,10 +88,11 @@ EOS
       description 'Show specified project'
       param :uuid, String, desc: 'The unique identifier for this project', required: true
       error 200, "Success"
-      error 404, "Project UUID couldn't be found"
+      error 404, "Project couldn't be found"
       example <<-EOS
 # Show specified project
 #{curl_method('projects/24c1de959cb943e0bf11e5ca6c8f8ad8', '1')}
+# =>
 {
   "project":{
     "uuid": "24c1de959cb943e0bf11e5ca6c8f8ad8",
@@ -106,15 +110,16 @@ EOS
       def show ; end
 
       api :PUT, '/projects/:uuid', 'Update a project'
-      description 'Update specified project params'
+      description 'Update specified project'
       param :uuid, String, desc: 'The unique identifier for this project', required: true
       param_group :project, as: :name
       error 200, "Updated project successfully"
-      error 404, "Project UUID couldn't be found"
+      error 404, "Project couldn't be found"
       error 422, "Failed validation. Details of failure returned in body."
       example <<-EOS
 # Rename project
 #{curl_method('projects/24c1de959cb943e0bf11e5ca6c8f8ad8', '1', {'X': 'PUT', 'd': '{"project": {"name": "wyld_stalyns_rock"}}'})}
+# =>
 {
   "project":{
     "uuid": "24c1de959cb943e0bf11e5ca6c8f8ad8",
@@ -132,14 +137,15 @@ EOS
       def update ; end
 
       api :DELETE, '/projects/:uuid', 'Delete project'
-      description 'Permanently removes the specified project.'
-      param :uuid, nil, desc: 'The unique identifier for this project', required: true
+      description 'Permanently remove the specified project.'
+      param :uuid, String, desc: 'The unique identifier for this project', required: true
       error 204, "Removed project successfully"
       error 404, "Project UUID couldn't be found"
       error 422, "Couldn't delete project. Details of failure returned in body."
       example <<-EOS
 # Delete project
 #{curl_method('projects/24c1de959cb943e0bf11e5ca6c8f8ad8', '1', {'X': 'DELETE', 'I': nil})}
+# =>
 HTTP/1.1 204 No Content
       EOS
       def destroy ; end
--- app/docs/harbour/v1/users_doc.rb
+++ app/docs/harbour/v1/users_doc.rb
@@ -14,61 +14,133 @@ module Harbour
       end
 
       def_param_group :user do
-        param :user, Hash, desc: 'User information', required: true do
-          param :email, String, desc: "User's email address", required: true
-          param :first_name, String, desc: "User's first name", required: false
-          param :last_name,  String, desc: "User's last name", required: false
+        param   :user,       Hash,   desc: 'User information',     required: true, action_aware: true do
+          param :email,      String, desc: "User's email address", required: true, as: :create
+          param :password,   String, desc: "User's password",      required: true, as: :create
+          param :first_name, String, desc: "User's first name"
+          param :last_name,  String, desc: "User's last name"
         end
       end
 
-      api :POST, '/users', 'Create user'
-      description 'Create user with specifed user params'
-      param_group :user
-      error code: 201, desc: "Created"
-      error code: 422, desc: "Validation failed"
+      api :POST, '/users', 'Create a new user'
+      description 'Create a new DataCentred user (backed by OpenStack).'
+      param_group :user, as: :create
+      error code: 201, desc: "Created successfully."
+      error code: 422, desc: "Failed validation. Details of failure returned in body."
       example <<-EOS
-#{curl_method('users', '1', {'X': 'POST', 'd': '{"user": {"email": "bill.s.preston@bogus.com", "first_name": "Bill S.", "last_name": "Preston Esq"}}'})}
+# Createw new user
+#{curl_method('users', '1', {'X': 'POST', 'd': '{"user": {"email": "bill.s.preston@bogus.com", "first_name": "Bill S.", "last_name": "Preston Esq", "password": "ilovejoanna"}}'})}
+# =>
+{
+  "user":{
+    "uuid": "4fd35cf93ff94a76ab206b11ae3d21e0",
+    "email": "bill.s.preston@bogus.com",
+    "first_name": "Bill S.",
+    "last_name": "Preston",
+    "links":[
+      {
+        "href": "http://localhost:3000/api/users/4fd35cf93ff94a76ab206b11ae3d21e0",
+        "rel": "self"
+      }
+    ],
+    "projects":[]
+  }
+}
       EOS
       def create ; end
 
-      api :GET, '/users', 'List users'
-      description <<-EOS
-        Foo
-      EOS
+      api :GET, '/users', 'List all available users'
+      description "Show a list of all the users"
+      error 200, "Success"
       example <<-EOS
-    #{curl_method('users','1') }
-    [
-      {
-        "full_name": "Foo",
-        "age": 25
-      }
-    ]
+#{curl_method('users','1')}
+# =>
+{
+  "users":[
+    {
+      "uuid": "4fd35cf93ff94a76ab206b11ae3d21e0",
+      "email": "bill.s.preston@bogus.com",
+      "first_name": "Bill S.",
+      "last_name": "Preston",
+      "links":[
+        {
+          "href": "http://localhost:3000/api/users/4fd35cf93ff94a76ab206b11ae3d21e0",
+          "rel": "self"
+        }
+      ],
+      "projects":[]
+    }
+  ]
+}
       EOS
       def index ; end
 
-      api :PUT, '/users/:uuid', 'Update user'
-      description 'Update specified user information'
-      param :uuid, String, required: true
-      param_group :user
-      error 404, "Missing"
+      api :GET, '/users/:uuid', 'Show user'
+      description 'Show specified user'
+      param :uuid, String, desc: 'The unique identifier for this user', required: true
+      error 200, "Success"
+      error 404, "User couldn't be found"
       example <<-EOS
-    #{curl_method('users/612bfb90f93c4b6e9ba515d51bb16022', '1', {'X': 'PUT', 'd': '{"user": {"foo": "bar"}}'})}
-      EOS
-      def update ; end
-
-      api :GET, '/users/:uuid', 'Get user'
-      description 'Retrieve specified user information'
-      param :uuid, String, required: true
-      example <<-EOS
-    #{curl_method('users/612bfb90f93c4b6e9ba515d51bb16022', '1')}
+# Show specified user
+#{curl_method('users/4fd35cf93ff94a76ab206b11ae3d21e0', '1')}
+# =>
+{
+  "user":{
+    "uuid": "4fd35cf93ff94a76ab206b11ae3d21e0",
+    "email": "bill.s.preston@bogus.com",
+    "first_name": "Bill S.",
+    "last_name": "Preston",
+    "links":[
+      {
+        "href": "http://localhost:3000/api/users/4fd35cf93ff94a76ab206b11ae3d21e0",
+        "rel": "self"
+      }
+    ],
+    "projects":[]
+  }
+}
       EOS
       def show ; end
 
-      api :DELETE, '/users/:uuid', 'Delete user'
-      description 'Remove specified user'
-      param :uuid, String, required: true
+      api :PUT, '/users/:uuid', 'Update a user'
+      description 'Update specified user'
+      param :uuid, String, desc: 'The unique identifier for this user', required: true
+      param_group :user, as: :update
+      error 200, "Updated user successfully"
+      error 404, "User couldn't be found"
+      error 422, "Failed validation. Details of failure returned in body."
       example <<-EOS
-    #{curl_method('users/612bfb90f93c4b6e9ba515d51bb16022', '1', {'X': 'DELETE'})}
+# Rename user
+#{curl_method('users/612bfb90f93c4b6e9ba515d51bb16022', '1', {'X': 'PUT', 'd': '{"user": {"last_name": "Preston Esq"}}'})}
+# =>
+{
+  "user":{
+    "uuid": "4fd35cf93ff94a76ab206b11ae3d21e0",
+    "email": "bill.s.preston@bogus.com",
+    "first_name": "Bill S.",
+    "last_name": "Preston Esq",
+    "links":[
+      {
+        "href": "http://localhost:3000/api/users/4fd35cf93ff94a76ab206b11ae3d21e0",
+        "rel": "self"
+      }
+    ],
+    "projects":[]
+  }
+}
+      EOS
+      def update ; end
+
+      api :DELETE, '/users/:uuid', 'Delete user'
+      description 'Permanently remove the specified user.'
+      param :uuid, String, desc: 'The unique identifier for this user', required: true
+      error 204, "Removed project successfully"
+      error 404, "Project UUID couldn't be found"
+      error 422, "Couldn't delete user. Details of failure returned in body."
+      example <<-EOS
+#{curl_method('users/4fd35cf93ff94a76ab206b11ae3d21e0', '1', {'X': 'DELETE', 'I': nil})}
+# =>
+HTTP/1.1 204 No Content
       EOS
       def destroy ; end
     end
