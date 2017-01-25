4a46a16fb4fb4e75fe89def2d94daea8abb334cb
Sean Handley <sean@dixiebook.home>
2017-01-25 17:48:25 +0000

    Better curl output.

--- app/helpers/harbour/curl_helper.rb
+++ app/helpers/harbour/curl_helper.rb
@@ -5,15 +5,19 @@ module Harbour
                              "Authorization: Token token=\"ACCESS_KEY:SECRET_KEY\"",
                              "Content-Type: application/json"
                             ]})
-      "curl '#{Harbour::Engine.config.public_url}/#{path}' \\\n #{to_args(params)}"
+      "curl '#{Harbour::Engine.config.public_url}/#{path}' \\\n#{to_args(params)}"
     end
 
     private
 
     def to_args(params)
       params.map do |k, v|
-        v.is_a?(Array) ? v.map {|e| "-#{k} '#{e}'"} : "-#{k} '#{v}'"
+        to_arg(k,v)
       end.flatten.join(" \\\n")
+    end
+
+    def to_arg(k,v)
+      v.is_a?(Array) ? v.map {|e| "-#{k} '#{e}'"} : "-#{k}".concat(v ? " '#{v}'" : '')
     end
   end
 end
