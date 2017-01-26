ed18f810d17a455f9f504b6b8e9124c59c5f0dc2
Sean Handley <sean@dixiebook.home>
2017-01-26 17:26:25 +0000

    Dynamic URL.

--- lib/harbour/engine.rb
+++ lib/harbour/engine.rb
@@ -4,12 +4,23 @@ module Harbour
 
     require 'responders'
 
+    def public_url
+      case Rails.env.development?
+      when 'production'
+        "https://my.datacentred.io/api"
+      when 'staging'
+        "https://staging-my.datacentred.io/api"
+      else
+        "http://localhost:3000/api"
+      end
+    end
+
     config.generators do |g|
       g.scaffold_controller :responders_controller
     end
 
     config.api_versions = [:V1]
     config.latest_api_version = config.api_versions.sort.last
-    config.public_url = Rails.env.development? ? "http://localhost:3000/api" : "https://my.datacentred.io/api"
+    config.public_url = public_url
   end
 end
