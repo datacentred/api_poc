74bc1227b603827d35aa7590551b376224d54c72
Sean Handley <sean.handley@datacentred.co.uk>
2017-04-27 19:24:26 +0100

    Tidy up.

--- test/integration/harbour/application_controller_test.rb
+++ test/integration/harbour/application_controller_test.rb
@@ -9,14 +9,18 @@ module Harbour
 
     test "enforces content type to be JSON for payloads" do
       params = {role: {name: 'Staff', permissions: ["usage.read", "tickets.modify"]}}
-      post '/api/roles', params: params.to_xml, headers: authorized_headers.merge("Content-Type" => "application/xml")
+      post '/api/roles', params: params.to_xml, headers: authorized_headers.merge(
+        "Content-Type" => "application/xml"
+      )
       assert_response :not_acceptable
       assert response_body["errors"][0]["detail"].include? 'json'
     end
 
     test "meaningful error for incorrect API versions" do
       get "/api/", params: { format: :dc_json }
-      get '/api/roles', headers: authorized_headers.merge("Accept" => "application/vnd.datacentred.api+json; version=999999999")
+      get '/api/roles', headers: authorized_headers.merge(
+        "Accept" => "application/vnd.datacentred.api+json; version=999999999"
+      )
       assert_response :not_acceptable
       assert response_body["errors"][0]["detail"].downcase.include? 'unknown api version'
     end
--- test/integration/harbour/version_controller_test.rb
+++ test/integration/harbour/version_controller_test.rb
@@ -4,9 +4,11 @@ module Harbour
   class VersionControllerTest < ApiTest
     test "shows available versions without authorization headers" do
       get "/api/", params: { format: :dc_json }
-      assert_equal Harbour::Engine.config.api_versions.count, response_body['versions'].count
+      assert_equal Harbour::Engine.config.api_versions.count,
+                   response_body['versions'].count
       current_version = response_body['versions'].find{|v| v['status'] == 'CURRENT'}
-      assert_equal Harbour::Engine.config.latest_api_version, "V#{current_version['id']}".to_sym
+      assert_equal Harbour::Engine.config.latest_api_version,
+                   "V#{current_version['id']}".to_sym
     end
   end
 end
