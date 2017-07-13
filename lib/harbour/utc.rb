12031d5cc5b62506b283dae5950b6fc3fa64eed7
Sean Handley <sean.handley@gmail.com>
2017-07-13 14:13:20 +0100

    Handle Time objects as well as strings.

--- lib/harbour/utc.rb
+++ lib/harbour/utc.rb
@@ -5,16 +5,16 @@ module Harbour
       
       if enum.is_a?(Array)
         enum.each_with_index do |element, i|
-          if element =~ time_format
-            enum[i] = Time.parse(element).utc.iso8601 rescue element
+          if element.is_a?(Time) || element =~ time_format
+            enum[i] = Time.parse(element.to_s).utc.iso8601 rescue element
           elsif element.is_a? Enumerable
             enum[i] = convert_dates_to_utc(element)
           end
         end
       elsif enum.is_a?(Hash)
         enum.each do |k,v|
-          if v =~ time_format
-            enum[k] = Time.parse(v).utc.iso8601 rescue v
+          if v.is_a?(Time) || v =~ time_format
+            enum[k] = Time.parse(v.to_s).utc.iso8601 rescue v
           elsif v.is_a? Enumerable
             enum[k] = convert_dates_to_utc(v)
           end
